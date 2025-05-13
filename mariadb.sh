6c075d07ffe643449f9893d8ae50529d6ab04410
mcmonde <mcmeliton1889@gmail.com>
2025-05-13 14:47:27 +0800

    add scripts for the crontab later.

---
+++ mariadb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/admin/website/laradock/.mariadb
+docker exec -i laradock_mariadb_1 mariadb-dump --user=$DB_USERNAME --password=$DB_PASSWORD wp_davao_city_staging > "/home/admin/website/laradock/.backups/db_name-backup-`date +%Y-%m-%d-%H:%M:%S`.sql"
+find /home/admin/website/laradock/.backups/*.sql -mtime +3 -type f -exec rm {} \;
