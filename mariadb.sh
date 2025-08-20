dfb3bd2d2dc8c932d743e31e7f522d449f76e4bf
mcmonde <mcmeliton1889@gmail.com>
2025-08-20 13:37:53 +0800

    updated scripts for backing up mysql, mariadb, and postgres.

--- mariadb.sh
+++ mariadb.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-source /home/admin/website/laradock/.mariadb
-docker exec -i laradock_mariadb_1 mariadb-dump --user=$DB_USERNAME --password=$DB_PASSWORD wp_davao_city_staging > "/home/admin/website/laradock/.backups/db_name-backup-`date +%Y-%m-%d-%H:%M:%S`.sql"
-find /home/admin/website/laradock/.backups/*.sql -mtime +3 -type f -exec rm {} \;
+
+#database type
+db_type=mysql
+
+# List of databases to back up
+databases=(
+    hris_db
+)
+
+# Loop through the databases and create backups
+for db in "${databases[@]}"; do
+    #docker exec -i laradock_mysql_1 mysqldump "$db" > "/home/laradock/.backups/${db_type}/${db}-backup-$(date +%Y-%m-%d-%H%M%S).sql"
+    docker exec -i laradock-mariadb-1 mariadb-dump "$db" > "/home/laradock/.backups/${db_type}/${db}-backup-$(date +%Y-%m-%d-%H%M%S).sql"
+done
+
+# Clean up old backup files
+find /home/laradock/.backups/${db_type}/*.sql -mtime +3 -type f -exec rm {} \;
